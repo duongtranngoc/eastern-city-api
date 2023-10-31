21c9a3b30d7dd59ec551ac9eb5c865091d8d09da
Trần Ngọc Dương <lotaynaongo@gmail.com>
2023-11-01 02:17:37 +0700

    created app base with spring boot

---
+++ src/main/java/com/example/easterncityapi/EasternCityApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.easterncityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasternCityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasternCityApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/easterncityapi/EasternCityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easterncityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasternCityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
